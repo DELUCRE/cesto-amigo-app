dd3b9615e054999cb41c9b1c30f0b0a24a60f46c
DELUCRE <denys.crespim@gmail.com>
2025-09-11 00:35:54 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.cesto.amigo',
-  appName: 'Cesto Amigo',
-  webDir: 'dist',
+  appId: 'com.cesto.amigo',   // Identificador único do app
+  appName: 'Cesto Amigo',     // Nome que aparecerá no celular
+  webDir: 'dist',             // Pasta onde ficam os arquivos buildados do seu front
   bundledWebRuntime: false
 };
 
