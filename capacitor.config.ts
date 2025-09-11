e731a4f817e17e480e4af8ff9abaf22e528c4194
DELUCRE <denys.crespim@gmail.com>
2025-09-11 00:34:57 -0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cesto.amigo',
+  appName: 'Cesto Amigo',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
