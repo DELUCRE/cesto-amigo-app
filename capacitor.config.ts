50fa4fc2c4cea0bb4497d9180587354719ba3dbe
DELUCRE <denys.crespim@gmail.com>
2025-09-13 11:37:16 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.cesto.amigo',
   appName: 'Cesto Amigo',
-  webDir: 'dist',
+  webDir: 'build',
   bundledWebRuntime: false
 };
 
