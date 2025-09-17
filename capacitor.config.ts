5b6227f568fa9b671f1ee9f73de5e5e53c9cc906
DELUCRE <denys.crespim@gmail.com>
2025-09-16 22:26:36 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,7 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
 const config: CapacitorConfig = {
-  appId: 'app.lovable.2d705dc2bbb4407880d1413db1b80727',
-  appName: 'cesto-amigo-app',
-  webDir: 'dist',
-  bundledWebRuntime: false,
-  server: {
-    url: "https://2d705dc2-bbb4-4078-80d1-413db1b80727.lovableproject.com?forceHideBadge=true",
-    cleartext: true
-  }
+  appId: "com.cestoamigo.app",
+  appName: "Cesto Amigo",
+  webDir: "dist",
+  bundledWebRuntime: false
 };
-
 export default config;
